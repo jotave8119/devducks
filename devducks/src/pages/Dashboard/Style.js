03abde9f57d9f371880e54a4fcfc2ccd145aa19c
jotave8119 <teixeirajoaovitor0@gmail.com>
2023-03-17 22:19:34 -0300

    commit

--- devducks/src/pages/Dashboard/Style.js
+++ devducks/src/pages/Dashboard/Style.js
@@ -4,7 +4,7 @@ export const DashboardContainer = styled.main`
 
 
     width: 100vw;
-    height: 100%;
+    min-height: 100vh;
     background-color: var(--Grey-3);
     display: flex;
     flex-direction: column;
